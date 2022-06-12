fe3c1186229460382a1a3a7dfdc688d5846f5c83
Tringa Krasniqi <tringe.krs@gmail.com>
2022-06-12 14:13:37 +0200

    How long did that take

--- Week 1/How long did that take/script.js
+++ Week 1/How long did that take/script.js
@@ -47,10 +47,20 @@ function rFib( n ) {
     }
     iFib(20);
 
-//O(1)
+//Both are O(1) but the recrusive on work faster
 
 //Reversing a string
 const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
 const reversed1 = story.split("").reverse().join("");
+
+//Reverse a string
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!');
 
 //O(1)
